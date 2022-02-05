2b1186b553c80f7636c116997d81b8612832380e
murkyweb <murkyweb@protonmail.com>
2022-02-05 12:35:01 +0200

    added button for reseting and changing size of grid

--- javascript.js
+++ javascript.js
@@ -1,17 +1,30 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
+let gridSize = 16;
 
-function createGrid () {
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+function createGrid (gridSize) {
+    gridSize = prompt('Enter a number: (MAX 35)', 16);
+    if (gridSize > 35) {
+        alert('Number must be 35 or lower!');
+        return;
+    } 
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
             const div = document.createElement('div');
+            div.setAttribute(
+                'style', `height: ${container.offsetHeight / gridSize}px;
+                width: ${container.offsetHeight / gridSize}px`
+                );
             div.classList.add('box');
             container.appendChild(div);
         }
     }
+    colorGrid();
 }
 
 function colorGrid () {
     const divs = document.querySelectorAll('.box');
+
     divs.forEach((div) => {
         div.addEventListener('mouseenter', () => {
             div.classList.add('color');
@@ -20,4 +33,10 @@ function colorGrid () {
 }
 
 createGrid();
-colorGrid();
+
+button.addEventListener('click', () => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    createGrid();
+});
